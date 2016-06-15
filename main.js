a4b10f945df7cb54bc943cfb378180ef59865442
Victor Rebiard--Crépin <victor.rebiardcrepin@viacesi.fr>
2016-06-15 23:15:11 +0200

    Clean code

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow // GC protection
+let mainWindow; // GC protection
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-  mainWindow.webContents.openDevTools()
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
