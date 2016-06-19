9993aecbe976bcac232a346627578cd03d33759f
Victor Rebiard--Crépin <victor.rebiardcrepin@viacesi.fr>
2016-06-20 00:17:30 +0200

    Cleanup

--- main.js
+++ main.js
@@ -6,25 +6,24 @@ let mainWindow; // GC protection
 
 global.args = process.argv;
 
-
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     mainWindow = null;
   });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
     createWindow();
   }
